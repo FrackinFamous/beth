abd86b0dc3ae41050b9106f413100722821218ff
frackinfamous <frackinfamous@gmail.com>
2024-02-28 16:59:39 -0500

    Add @elysiajs/html dependency and update index.ts to use HTML template

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia()
+  .use(html())
+  .get("/", () => baseHtml)
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+const baseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Elysia Beth Stack</title>
+</head>
+<body>Elysia Beth Stack</body>
+</html>
+`;
